8d9417eaf51472c16de93f73dea2baad387be2e8
Denis Plisko <denpliss@gmail.com>
2015-09-03 14:02:30 +0300

    Update Build.

--- dist-modules/utils/extractNativeItem.js
+++ dist-modules/utils/extractNativeItem.js
@@ -4,15 +4,29 @@ var isFileDragDropEvent = require("./isFileDragDropEvent"),
     isUrlDragDropEvent = require("./isUrlDragDropEvent");
 
 function extractNativeItem(e) {
-  if (isFileDragDropEvent(e)) {
-    return {
-      files: Array.prototype.slice.call(e.dataTransfer.files)
-    };
-  } else if (isUrlDragDropEvent(e)) {
-    return {
-      urls: (e.dataTransfer.getData("Url") || e.dataTransfer.getData("text/uri-list") || "").split("\n")
-    };
-  }
+    if (isFileDragDropEvent(e)) {
+        var entries = [];
+        var ex = "";
+        try {
+            Array.prototype.slice.call(e.dataTransfer.items).forEach(function (it) {
+                entries.push(it.webkitGetAsEntry());
+            });
+        } catch (err) {
+            console.log(err);
+            ex = err;
+        }
+
+        return {
+            files: Array.prototype.slice.call(e.dataTransfer.files),
+            items: Array.prototype.slice.call(e.dataTransfer.items),
+            entries: entries,
+            ex: ex
+        };
+    } else if (isUrlDragDropEvent(e)) {
+        return {
+            urls: (e.dataTransfer.getData("Url") || e.dataTransfer.getData("text/uri-list") || "").split("\n")
+        };
+    }
 }
 
 module.exports = extractNativeItem;
